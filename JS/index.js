104d8e72a241ac24b9b9c2723468407d5f5687c0
j23063519 <j23063519@gmail.com>
2021-01-13 18:59:15 +0800

    up01131859

--- JS/index.js
+++ JS/index.js
@@ -35,3 +35,32 @@ a_top.addEventListener("click", function () {
     behavior: 'smooth',
   })
 });
+
+// 登入註冊
+$('a#loginLogout').on('click', function () {
+  $('div#loginlogout').toggleClass('actopa');
+});
+
+let prism = document.querySelector(".rec-prism");
+
+function showSignup() {
+  prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
+}
+function showLogin() {
+  prism.style.transform = "translateZ(-100px)";
+}
+function showForgotPassword() {
+  prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
+}
+
+function showSubscribe() {
+  prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
+}
+
+function showContactUs() {
+  prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
+}
+
+function showThankYou() {
+  prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
+}
